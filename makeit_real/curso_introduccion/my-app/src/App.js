b622986892367307e68e8fd59c6b2a97769c2155
danielmoralesp <daniel_moralesp@hotmail.com>
2018-02-05 11:21:14 -0500

    Recorrer un array en JSX

--- makeit_real/curso_introduccion/my-app/src/App.js
+++ makeit_real/curso_introduccion/my-app/src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const names = ["pedro", "juan", "daniel"];
     return (
       <div>
         <h1>Hola Mundo</h1>
         <p>Hola</p>
+        <ul>
+          {names.map(name =>
+            <li>{name}</li>
+          )}
+        </ul>
       </div>
     );
   }
