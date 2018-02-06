3dc833014d1b62b56097af0971fe681296ab1027
danielmoralesp <daniel_moralesp@hotmail.com>
2018-02-06 17:53:24 -0500

    Despliegue del primer codigo en el navegador

--- platzi/fromScratch/index.js
+++ platzi/fromScratch/index.js
@@ -1,1 +1,10 @@
-console.log("Hola Mundo")
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('app')
+const holaMundo = <h1>Hola Mundo!</h1>
+
+ReactDOM.render(
+  holaMundo,
+  app
+)
