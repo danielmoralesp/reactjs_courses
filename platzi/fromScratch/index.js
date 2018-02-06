e1d8f42a633766a1b57cdd072d9ba844c7c1241b
danielmoralesp <daniel_moralesp@hotmail.com>
2018-02-06 17:57:01 -0500

    From dev build

--- platzi/fromScratch/index.js
+++ platzi/fromScratch/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const app = document.getElementById('app')
-const holaMundo = <h1>Hola Mundo!</h1>
+const holaMundo = <h1>Hola Mundo estudiante!</h1>
 
 ReactDOM.render(
   holaMundo,
