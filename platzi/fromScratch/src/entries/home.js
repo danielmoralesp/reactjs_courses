d345691b03dec11fa5cb68d4cd2e39a27808ad4d
danielmoralesp <daniel_moralesp@hotmail.com>
2018-02-07 11:42:45 -0500

    Project Structure with Containers and UI Components: Categories

--- platzi/fromScratch/src/entries/home.js
+++ platzi/fromScratch/src/entries/home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
+import data from '../api.json';
 
 const app = document.getElementById('app')
 
 render(
-  <Home />,
+  <Home data={data} />,
   app
 );
