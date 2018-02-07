d509d2e81fb34e47cbe45972c47515ebc8d87294
danielmoralesp <daniel_moralesp@hotmail.com>
2018-02-07 10:19:15 -0500

    Pure and functional components

--- platzi/fromScratch/src/playlist/components/playlist.js
+++ platzi/fromScratch/src/playlist/components/playlist.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Media from './media.js'
 import './playlist.css';
 
-class Playlist extends Component {
-  render() {
-    const playlist = this.props.data.categories[0].playlist
-    console.log(this.props.data)
+function Playlist(props) {
+    const playlist = props.data.categories[0].playlist
+    console.log(props.data)
     return(
       <div className="Playlist">
         {
           playlist.map((item) => {
             return <Media {...item} key={item.id} />
-            // return <Media title={item.title} key={item.id} />
           })
         }
       </div>
     )
-  }
 }
 
 export default Playlist
