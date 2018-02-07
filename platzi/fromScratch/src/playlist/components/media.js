eb904a79656b5934d472de3fbd3edb1048873c0b
danielmoralesp <daniel_moralesp@hotmail.com>
2018-02-06 20:51:44 -0500

    primer componente sencillo

--- platzi/fromScratch/src/playlist/components/media.js
+++ platzi/fromScratch/src/playlist/components/media.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 class Media extends React.Component{
   render(){
+    const styles = {
+      container: {
+        color: '#44546b',
+        cursor: 'pointer',
+        width: 260,
+        border: '1px solid red'
+      }
+    }
     return(
-      <div>
+      <div style={styles.container}>
         <div>
           <img
-              src=""
+              src="./images/covers/bitcoin.jpg"
               alt=""
               width={260}
               height={160}
