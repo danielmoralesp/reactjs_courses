2344a7130e31120a410cd229c601d78b374ea71f
danielmoralesp <daniel_moralesp@hotmail.com>
2018-02-06 20:37:25 -0500

    First component

--- platzi/fromScratch/index.js
+++ platzi/fromScratch/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Media from './src/playlist/components/media';
 
 const app = document.getElementById('app')
-const holaMundo = <h1>Hola Mundo estudiante!</h1>
+// const holaMundo = <h1>Hola Mundo estudiante!</h1>
 
 ReactDOM.render(
-  holaMundo,
+  <Media />,
   app
 )
