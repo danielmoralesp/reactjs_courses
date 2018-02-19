52cd89259a6b95a70e7a10e976cb41e87e0ec7d4
danielmoralesp <daniel_moralesp@hotmail.com>
2018-02-19 09:24:53 -0500

    Props

--- makeit_real/curso_introduccion/my-app2/src/App.js
+++ makeit_real/curso_introduccion/my-app2/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Welcome from './Welcome'
 import './App.css'
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <h1>Hola mundo</h1>
+        <Welcome name="felipe" />
         <ul>
           {names.map(name => {
             return <li>{name}</li>
