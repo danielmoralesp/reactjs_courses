dda76f716436f9c4ae3dea189d7881d3bc299ac4
danielmoralesp <daniel_moralesp@hotmail.com>
2018-02-19 08:57:42 -0500

    JSX sencillo

--- makeit_real/curso_introduccion/my-app2/src/App.js
+++ makeit_real/curso_introduccion/my-app2/src/App.js
@@ -3,7 +3,17 @@ import './App.css'
 
 class App extends Component {
   render() {
-    return <h1>Hola Mundo</h1>
+    const names = ['daniel', 'maria clara', 'catalina']
+    return (
+      <div>
+        <h1>Hola mundo</h1>
+        <ul>
+          {names.map(name => {
+            return <li>{name}</li>
+          })}
+        </ul>
+      </div>
+    )
   }
 }
 
