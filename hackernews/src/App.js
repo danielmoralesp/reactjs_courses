6968f59becdb6d8a46dde9aebbe98cafc3d6cc13
danielmoralesp <daniel_moralesp@hotmail.com>
2018-02-21 10:20:53 -0500

    Split up components

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -54,20 +54,39 @@ class App extends Component {
   }
 
   render() {
-    const helloWorld = 'Welcome to the road to learn React'
-    const { list, searchTerm } = this.state
+    const { searchTerm, list } = this.state
 
     return (
       <div className="App">
+        <Search value={searchTerm} onChange={this.onSearchChange} />
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+      </div>
+    )
+  }
+}
+
+class Search extends Component {
+  render() {
+    const helloWorld = 'Welcome to the road to learn React'
+    const { value, onChange } = this.props
+
+    return (
+      <div>
         <h2>{helloWorld}</h2>
         <form>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={this.onSearchChange}
-          />
+          <input type="text" value={value} onChange={onChange} />
         </form>
-        {list.filter(isSearched(searchTerm)).map(item => (
+      </div>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -76,10 +95,7 @@ class App extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
+              <button onClick={() => onDismiss(item.objectID)} type="button">
                 Dismiss
               </button>
             </span>
