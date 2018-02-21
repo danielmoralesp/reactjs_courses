20d8c6c8945f2c312782d4b222186aa66585b440
danielmoralesp <daniel_moralesp@hotmail.com>
2018-02-21 08:59:34 -0500

    Destructuring

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -55,13 +55,15 @@ class App extends Component {
 
   render() {
     const helloWorld = 'Welcome to the road to learn React'
+    const { list, searchTerm } = this.state
+
     return (
       <div className="App">
         <h2>{helloWorld}</h2>
         <form>
           <input type="text" onChange={this.onSearchChange} />
         </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => (
+        {list.filter(isSearched(searchTerm)).map(item => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
