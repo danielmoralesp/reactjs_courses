bad09ee719fbd241d20993e568f0fe0201578f8d
danielmoralesp <daniel_moralesp@hotmail.com>
2018-08-06 16:25:42 -0500

    React State - Handling user form input

--- rails_react_calendar/app/assets/javascripts/components/appoinment_form.jsx
+++ rails_react_calendar/app/assets/javascripts/components/appoinment_form.jsx
@@ -1,11 +1,28 @@
 class AppointmentForm extends React.Component {
+  handleChange(e) {
+    var name = e.target.name
+    obj = {}
+    obj[name] = e.target.value
+    this.props.onUserInput(obj)
+  }
+
   render() {
     return (
       <div>
         <h2>Make a new appointment</h2>
         <form>
-          <input name="title" placeholder="Appointment Title" />
-          <input name="appt_time" placeholder="Date and Time" />
+          <input
+            name="title"
+            placeholder="Appointment Title"
+            value={this.props.input_title}
+            onChange={this.handleChange}
+          />
+          <input
+            name="appt_time"
+            placeholder="Date and Time"
+            value={this.props.input_app_time}
+            onChange={this.handleChange}
+          />
           <input type="submit" value="Make Appointment" />
         </form>
       </div>
--- rails_react_calendar/app/assets/javascripts/components/appointments.jsx
+++ rails_react_calendar/app/assets/javascripts/components/appointments.jsx
@@ -1,9 +1,26 @@
 class Appointments extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      appointments: this.props.appointments,
+      input_title: 'Team standupp meeting',
+      input_app_time: 'Tomorrow at 9am'
+    }
+  }
+
+  handleUserInput(obj) {
+    this.setState(obj)
+  }
+
   render() {
     return (
       <div>
-        <AppointmentForm />
-        <AppointmentsList appointments={this.props.appointments} />
+        <AppointmentForm
+          input_title={this.state.input_title}
+          input_app_time={this.state.input_app_time}
+          onUserInput={this.handleUserInput}
+        />
+        <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
   }
